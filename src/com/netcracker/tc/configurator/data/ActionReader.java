a467d92d81bd4d1114c24e918cd5de8e29192848
Aleksej Dsiuba <atamur@yandex.ru>
2012-03-16 13:41:44 +0200

    Deleting removed files

--- src/com/netcracker/tc/configurator/data/ActionReader.java
+++
@@ -1,8 +0,0 @@
-package com.netcracker.tc.configurator.data;
-
-import com.netcracker.tc.model.ActionGroup;
-
-public interface ActionReader
-{
-    ActionGroup readActionGroup() throws DataException;
-}
--- src/com/netcracker/tc/configurator/data/SchemaReader.java
+++
@@ -1,8 +0,0 @@
-package com.netcracker.tc.configurator.data;
-
-import com.netcracker.tc.model.ActionGroup;
-
-public interface SchemaReader
-{
-    ActionGroup readActionGroup() throws DataException;
-}
